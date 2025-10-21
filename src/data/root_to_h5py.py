d369e821d519846cf947a24085b761ceb81559cf
jackgoodsall <jack.goodsall@gmail.com>
2025-10-21 15:23:45 +0100

    Moved over to use global yaml config

--- src/data/root_to_h5py.py
+++ src/data/root_to_h5py.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 
 import os
 
@@ -10,15 +10,12 @@ import h5py
 from torch.utils.data import random_split
 import uproot
 import torch
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from src.utils.utils import  load_and_split_config
+config_file_path = "config/transformer_classifier_config.yaml"
 
-DATA_DIR = "data/fourtopvsthree/rootfiles/"
 
-
-class FileConfig:
-    files = {DATA_DIR + "3tJ_LO_final.root" : 0,
-              DATA_DIR+ "3tWm_LO_final.root" : 0,
-             DATA_DIR +   "3tWp_LO_final.root" : 0, 
-             DATA_DIR +    "4top_2LSS_April18.root" : 1}
 
 PARTICLE_PREFIXES = {
     "jet" : 0,
@@ -28,26 +25,32 @@ PARTICLE_PREFIXES = {
 }
 
 class TopClassiferDataSetPrepaper:
-    file_config = FileConfig
-    def __init__(self):
+
+    def __init__(self, config):
+        self.config = config.data_pipeline["root_to_h5py"]
+        raw_data_dir = self.config["raw_data_dir"]
+        self.target_file_dict = { **{ raw_data_dir + file : 1 for file in  self.config["signal_files"] },
+                                 **{raw_data_dir + file : 0 for file in self.config["background_files"] 
+        }}
         (
          particle_data, 
          global_data, 
          src_mask,
          targets
         ) = self.parse_root_file(
-             max_particles = 25,
-             particle_features = ["_pt", "_eta", "_phi", "_mass"],
-             global_features = ["met_met", "met_eta", "met_phi"]
+             max_particles = self.config["max_particles"],
+             particle_features = self.config["particle_features"],
+             global_features = self.config["global_features"]
                 )  
         self.particle_data = particle_data
         self.global_data = global_data
         self.targets = targets
+        
     
     def parse_root_file(self, max_particles, particle_features, global_features):
         per_file_events, per_file_targets, per_file_globals = [], [], []
 
-        for path, y in self.file_config.files.items():
+        for path, y in self.target_file_dict.items():
             reco = uproot.open(path)["Reco;1"]
             blocks = []
 
@@ -59,7 +62,7 @@ class TopClassiferDataSetPrepaper:
                 feats = [f"{prefix}{fe}" for fe in particle_features]
                 bdict = reco.arrays(feats, how=dict)
                 base  = ak.concatenate([bdict[name][..., None] for name in feats], axis=-1)
-
+                # Comment charge out for now
                 if prefix in ("el", "mu"):
                     chd = reco.arrays(f"{prefix}_charge", how=dict)
                     ch  = ak.concatenate([chd[k][..., None] for k in chd], axis=-1)
@@ -72,7 +75,7 @@ class TopClassiferDataSetPrepaper:
                 else:
                     bt = ak.zeros_like(base[..., :1])
 
-                blocks.append(ak.concatenate([base, ch, bt], axis=-1))
+                blocks.append(ak.concatenate([base,  bt], axis=-1))
 
             events = ak.concatenate(blocks, axis=1)
             per_file_events.append(events)
@@ -100,10 +103,12 @@ class TopClassiferDataSetPrepaper:
     def _save_dataset(self, save_file_name: str):
         ## Assumes the data set is already created tbh cba coding in the checks
         with h5py.File(save_file_name, "w") as f:
-            dset = f.create_dataset("particle_data", data = np.array(self.particle_data))
-            glo_dset = f.create_dataset("global_features" , data = np.array(self.global_data))
-            target_dset = f.create_dataset("targets", data = np.array(self.targets))
+            dset = f.create_dataset("particle_data", data = self.particle_data)
+            glo_dset = f.create_dataset("global_features" , data = self.global_data)
+            target_dset = f.create_dataset("targets", data = self.targets)
 
 
-data_set = TopClassiferDataSetPrepaper()
-data_set._save_dataset("all_combined_raw")
+if __name__ == "__main__":
+    config = load_and_split_config(config_file_path)
+    data_set = TopClassiferDataSetPrepaper(config)
+    data_set._save_dataset(config.data_pipeline["root_to_h5py"]["file_save_name"])
