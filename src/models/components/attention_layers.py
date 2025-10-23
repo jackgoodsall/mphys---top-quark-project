8d11e9fcafbde69c660ebc531f723c0a20bbd200
jackgoodsall <jack.goodsall@gmail.com>
2025-10-23 10:58:35 +0100

    Started working in implementiog transformer blocks from ParT paper

---
+++ src/models/components/attention_layers.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch
+import torch.functional as F
+
+class ParticleAttentionBlock(nn.Module):
+    """
+    Implements the Particle Attention block decribed in the paper https://arxiv.org/abs/2202.03772 
+
+    """
+    def __init__(self):
+
+
+class ClassAttentionBlock(nn.module):
+    """
+    Implements the class attention block desribed in the paper  https://arxiv.org/abs/2202.03772 
+    """
