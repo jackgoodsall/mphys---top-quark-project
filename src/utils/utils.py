105ee86b4c958f62e3f99310ba7ec04b22999b23
jackgoodsall <jack.goodsall@gmail.com>
2025-10-28 11:21:02 +0000

    Adedd helper function for loading any config

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -13,6 +13,7 @@ class BaseConfig(BaseModel):
     data: DataConfig
     model_parameters: Dict[str, Any]
     train: Dict[str, Any]
+    data_pipeline: Dict[str, Any]
 
 def load_and_split_config(config_input_file: str) -> BaseConfig:
 
@@ -20,3 +21,8 @@ def load_and_split_config(config_input_file: str) -> BaseConfig:
         raw_config = yaml.safe_load(f)
     cfg = BaseConfig(**raw_config)
     return cfg
+
+def load_any_config(config_input_file: str) -> Dict[str: Any]:
+    with open(config_input_file, "r") as f
+        raw_config = yaml.safe_load(f)
+    return f
