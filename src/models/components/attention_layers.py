3f22c0c43253e3bf3d18273399da90d2261c0482
jackgoodsall <jack.goodsall@gmail.com>
2025-11-20 11:13:44 +0000

    Added something?

--- src/models/components/attention_layers.py
+++ src/models/components/attention_layers.py
@@ -84,7 +84,8 @@ class ParticleAttentionBlock(nn.Module):
         self.embedded_dim = embedded_dim
         self.feed_for_dim = feed_for_dim
         self.n_heads = n_heads
-        assert embedded_dim // n_heads  == 0 ; "nheads needs to be a factor of embedded dimension"
+
+        assert embedded_dim % n_heads  == 0 ; "nheads needs to be a factor of embedded dimension"
         self.head_dim  = embedded_dim // n_heads 
 
         self.attn_dropout = attn_dropout
@@ -94,7 +95,8 @@ class ParticleAttentionBlock(nn.Module):
         ## Attention Layers
         self.ln1 = nn.LayerNorm(embedded_dim)
         self.qkv = nn.Linear(embedded_dim, embedded_dim * 3) # wq, wk, wv
-        
+        self.out_proj = nn.Linear(embedded_dim, embedded_dim)
+
         self.ln2 = nn.LayerNorm(embedded_dim)
         ## Feedforward residual area
         self.feed_forward_block = nn.Sequential(
@@ -111,7 +113,7 @@ class ParticleAttentionBlock(nn.Module):
         head_dim = self.head_dim
         x1 = self.ln1(x) # [B, N_part, embedded]
         qkv = self.qkv(x1) # [B, N_part, embedded * 3]
-        qkv = qkv.view(B, T, H, head_dim, 3).tranponse(1, 2)  # [B, N_head, N_particle, N_head_dim, 3]
+        qkv = qkv.view(B, T, H, head_dim, 3).transpose(1, 2)  # [B, N_head, N_particle, N_head_dim, 3]
         q, k , v = qkv[..., 0],qkv[..., 1],qkv[..., 2] 
 
         ## Assumes by construction that u -> the interaction is padded to -inf where
@@ -123,7 +125,11 @@ class ParticleAttentionBlock(nn.Module):
             dropout_p = self.attn_dropout,
             is_causal = False)
         
-        post_attention_scores = self.ln2(attention_scores) + x
+        attn = attention_scores.transpose(1, 2).contiguous().view(B, T, self.embedded_dim)
+        attn = self.out_proj(attn)
+
+
+        post_attention_scores = self.ln2(attn) + x
         residual = self.feed_forward_block(post_attention_scores) + post_attention_scores
         return residual
 
@@ -132,14 +138,14 @@ class ClassAttentionBlock(nn.Module):
     """
     Implements the class attention block desribed in the paper  https://arxiv.org/abs/2202.03772 
     """
-    def __init__(self, embedded_dim, feed_for_dim, 
-                 n_heads, p_dropout):
+    def __init__(self, embedded_dim,n_heads, feed_for_dim, 
+                  p_dropout):
         super().__init__()
 
         self.embedded_dim = embedded_dim
         self.feed_for_dim = feed_for_dim
         self.n_heads = n_heads
-        assert embedded_dim // n_heads  == 0 ; "nheads needs to be a factor of embedded dimension"
+        assert embedded_dim % n_heads  == 0 ; "nheads needs to be a factor of embedded dimension"
         self.head_dim  = embedded_dim // n_heads 
         self.p_dropout = p_dropout
 
@@ -149,7 +155,8 @@ class ClassAttentionBlock(nn.Module):
 
 
         self.query_linear = nn.Linear(embedded_dim, embedded_dim)
-
+        self.attention = nn.MultiheadAttention(embedded_dim, n_heads, p_dropout, batch_first= True)
+        self.ln2 = nn.LayerNorm(embedded_dim)
         ## Feedforward block
         self.feed_forward_block = nn.Sequential(
         nn.LayerNorm(self.embedded_dim),
@@ -160,13 +167,24 @@ class ClassAttentionBlock(nn.Module):
         )
     
     def forward(self, x, cls_tkn, src_mask = None):
-        """ inital_input = torch.concat((x, cls_tkn), axis = 1)
+        B, N, _ = x.shape
+        inputs = torch.concat((x, cls_tkn), axis = 1)
+        if src_mask is not None:
+            src_mask = torch.concat((src_mask, torch.zeros(B, 2).to(x.device)), axis = 1)
+        attention_input = self.ln1(inputs)
         
-        q = self.query_linear(cls_tkn)
+        attention_scores, _ = self.attention(
+            cls_tkn,
+            attention_input,
+            attention_input,
+            key_padding_mask = src_mask
+        )
 
-        attention_scores = F.scaled_dot_product_attention( 
-            q, k, v, 
-            attn_mask = ,
-            dropout_p = self.attn_dropout,
-            is_causal = False) """
-        raise NotImplementedError
+        
+        attn = attention_scores + cls_tkn
+
+
+        post_attention_scores = self.ln2(attn) 
+
+        residual = self.feed_forward_block(post_attention_scores) + post_attention_scores
+        return residual
