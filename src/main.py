e1c76585b631a86c9d4603b885fb83c26148331a
jackgoodsall <jack.goodsall@gmail.com>
2026-02-04 14:14:51 +0000

    Removed un needed code

--- src/main.py
+++ src/main.py
@@ -36,18 +36,3 @@ if __name__ == "__main__":
                                                 reconstruct_Ws = config["reconstruct_W"])
     trainer.test(model, datamodule = topantitopquark)
 
-
-## Random place holder to remember the classication pipeline running 
-if __name__ == "Fourtop_classifcation":
-    config = load_and_split_config("config/transformer_classifier_config.yaml")
-    classifer_head = ParticleBinaryClassificaitionHead(**config.model_parameters["classifer_head"])
-    config.model_parameters["transformer"]["classifcation_head"] = classifer_head
-    transformer_model = ParticleTransformer(**config.model_parameters["transformer"])
-    fourtop_dataset = FourvsThreeTopDataModule(**config.data.train)
-    trainer, model  = train_binary_classifier_model(transformer_model, 
-                                   fourtop_dataset,
-                                   config.train,
-                                   config.train["min_epochs"],
-                                   config.train["max_epochs"],
-                                   )
-    trainer.test(model, datamodule= fourtop_dataset)
